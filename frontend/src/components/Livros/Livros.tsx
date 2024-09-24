75c2a34581ed66ad7e9a70c1317b4b0a46203e68
ThalitaBispo <thallitaasoares@gmail.com>
2024-09-24 20:20:40 -0300

    Consertos

--- frontend/src/components/Livros/Livros.tsx
+++ frontend/src/components/Livros/Livros.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 import apilivros from '../services/ApiLivros';
 
 export function Livros() {
